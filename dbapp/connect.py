fececfe7a9f80204121d466d4538b8a03bde7bc1
MANJU DAVIS <123202668+ManjuAlwin-18@users.noreply.github.com>
2023-03-16 13:19:36 +1300

    Update connect.py
    Updated Password

--- dbapp/connect.py
+++ dbapp/connect.py
@@ -1,5 +1,5 @@
 dbuser = "root" #PUT YOUR MySQL username here - usually admin
-dbpass = "@lw1N@777" #"PUT YOUR PASSWORD HERE"
+dbpass = "" #"PUT YOUR PASSWORD HERE"
 dbhost = "localhost" #"PUT YOUR AWS Connect String here"
 dbport = "3306"
 dbname = "airline"
